5197c9668e78e8e627a5919f8f2cb166d876e903
ZJaume <jzaragoza@prompsit.com>
2023-04-13 13:59:26 +0300

    Check needed env variables are defined before running jobs

--- 00.merge-batching.sh
+++ 00.merge-batching.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 source .env
+source .checks
 
 L=$1
 COLLECTIONS=${@:2}
+mkdir -p $SBATCH_OUTPUT
 
+SBATCH_OUTPUT="$SBATCH_OUTPUT/%x.out" \
 sbatch -J merge-batching-$L 00.merge-batching $L $COLLECTIONS
