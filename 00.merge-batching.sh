99eaaed2193381edad42cf31649da876e92875e6
ZJaume <jzaragoza@prompsit.com>
2023-04-18 18:43:33 +0300

    Run all script (does not work)
    We cannot know the size of array jobs before merge-batching is finished
    :S

--- 00.merge-batching.sh
+++ 00.merge-batching.sh
@@ -8,4 +8,4 @@ COLLECTIONS=${@:2}
 mkdir -p $SLURM_LOGS_DIR
 
 SBATCH_OUTPUT="$SLURM_LOGS_DIR/%x.out" \
-sbatch -J $L-merge-batching 00.merge-batching $L $COLLECTIONS
+sbatch -J $L-merge-batching --parsable 00.merge-batching $L $COLLECTIONS
--- 10.processing.sh
+++ 10.processing.sh
@@ -10,4 +10,5 @@ ARRAY_SIZE=$(ls -1 $WORKSPACE/$L/batch.*.zst | sed -E 's#.*/batch\.([0-9]+)\.zst
 SBATCH_OUTPUT="$SLURM_LOGS_DIR/%x-%A_%a.out" \
 sbatch --array=1-$ARRAY_SIZE \
     -J $L-mono-processing \
+    --parsable \
     10.processing $L
