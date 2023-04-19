32069b4d5fad118b77f85403300df56527458d1e
ZJaume <jzaragoza@prompsit.com>
2023-04-19 11:29:04 +0300

    Re-run failed tasks of 10.processing.sh job arrays

--- 10.processing.sh
+++ 10.processing.sh
@@ -4,11 +4,21 @@ source .env
 source .checks
 
 L=$1
+INDEX=$2
 
-ARRAY_SIZE=$(ls -1 $WORKSPACE/$L/batch.*.zst | sed -E 's#.*/batch\.([0-9]+)\.zst#\1#' | sort -n | tail -1)
+if [ "$INDEX" == "all" ]; then
+    INDEX=1-$(ls -1 $WORKSPACE/$L/batch.*.zst | sed -E 's#.*/batch\.([0-9]+)\.zst#\1#' | sort -n | tail -1)
+elif [ "$INDEX" == "failed" ]; then
+    JOB=$3
+    INDEX=$(
+        sacct -j $JOB --parsable -s oom,f,to -n \
+        | grep -v '.batch' \
+        | sed -E 's/[0-9]+_([0-9]+)\|.*/\1/g' \
+        | paste -sd','
+    )
+fi
 
 SBATCH_OUTPUT="$SLURM_LOGS_DIR/%x-%A_%a.out" \
-sbatch --array=1-$ARRAY_SIZE \
+sbatch --array=$INDEX \
     -J $L-mono-processing \
-    --parsable \
-    10.processing $L
+    --parsable 10.processing $L
