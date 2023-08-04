ac4ca73ee8306136c6c13e9923551d496d01f4a2
ZJaume <jzaragoza@prompsit.com>
2023-08-04 10:11:57 +0000

    Test for mhindex and dedup using CLI

---
+++ utils/tests/cli.rs
@@ -0,0 +1,28 @@
+use std::io::Write;
+use std::fs;
+use tempfile::NamedTempFile;
+use assert_cmd::Command;
+use zstd::bulk::compress;
+
+// Test that checks index and dedup of a sample of documents
+#[test]
+fn mhindex_dedup() -> Result<(), Box<dyn std::error::Error>> {
+    let expected_output = fs::read_to_string("tests/dedup.out")?;
+    let mut mhindex_cmd = Command::cargo_bin("mhindex")?;
+
+    let mhindex_out = mhindex_cmd.arg("tests/sample.jsonl.zst")
+        .output().unwrap().stdout;
+    let mut temp = NamedTempFile::new()?;
+    temp.write_all(&compress(&mhindex_out, 0)?)?;
+
+    let mut dedup_cmd = Command::cargo_bin("dedup")?;
+    dedup_cmd
+        .arg(temp.path())
+        .arg("tests/sample.jsonl.zst")
+        .assert()
+        .success()
+        .stdout(expected_output);
+
+    Ok(())
+}
+
