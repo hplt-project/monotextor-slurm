14f67c9aaac9c1d7a9241a264ce3c5505b6c11b8
ZJaume <jzaragoza@prompsit.com>
2023-08-04 16:10:17 +0300

    Dedup jobs with or wiehout distributed index

--- 20.dedup.sh
+++ 20.dedup.sh
@@ -10,16 +10,30 @@ COLL=$2
 mkdir -p $SLURM_LOGS_DIR
 echo ${COLLECTIONS[$COLL]}/$L
 
-bands=$(mhindex -d -p 260 -j 0.8 2>&1 | grep 'Num bands' | perl -pe 's/.*Num bands: (\d+)/$1/')
-echo Submitting array job of 1-$bands jobs
-read -p "Confirm? [y/n] " -n 1 -r
-if [[ ! $REPLY =~ [Yy] ]]; then echo; exit 1; fi
-echo
+# Distributed minhash index
+if [[ $# -eq 3 ]] && [[ $3 =~ "dist" ]]; then
+    bands=$(mhindex -d -p 260 -j 0.8 2>&1 | grep 'Num bands' | perl -pe 's/.*Num bands: (\d+)/$1/')
+    echo Submitting array job of 1-$bands jobs
+    read -p "Confirm? [y/n] " -n 1 -r
+    if [[ ! $REPLY =~ [Yy] ]]; then echo; exit 1; fi
+    echo
 
-INDEX_ID=$(\
-    SBATCH_OUTPUT="$SLURM_LOGS_DIR/%x-%A_%a.out" \
-    sbatch -J $L-$COLL-index --array=1-$bands \
-    --parsable 20.index $L $COLL)
+    INDEX_ID=$(\
+        SBATCH_OUTPUT="$SLURM_LOGS_DIR/%x-%A_%a.out" \
+        sbatch -J $L-$COLL-index --array=1-$bands \
+        --parsable 20.index $L $COLL)
+else
+    echo Submitting job
+    read -p "Confirm? [y/n] " -n 1 -r
+    if [[ ! $REPLY =~ [Yy] ]]; then echo; exit 1; fi
+    echo
+
+    INDEX_ID=$(\
+        SBATCH_OUTPUT="$SLURM_LOGS_DIR/%x.out" \
+        sbatch -J $L-$COLL-index \
+        --parsable 20.index $L $COLL)
+fi
+
 echo "Submitted batch job $INDEX_ID"
 
 SBATCH_OUTPUT="$SLURM_LOGS_DIR/%x.out" \
