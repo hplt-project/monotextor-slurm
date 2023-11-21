c70241d44168c5525825ddec698860d39b7aa1ed
ZJaume <jzaragoza@prompsit.com>
2023-11-21 13:02:08 +0200

    Fix re-submission of failed jobs for clean

--- 30.clean.sh
+++ 30.clean.sh
@@ -14,13 +14,13 @@ L=$1
 if [ $# -lt 2 ] || [ "$2" == "all" ]; then
 # List all the batches that need to be processed (size of the job array)
     INDEX=1-$(ls -1 $WORKSPACE/dedup/$L/${L}_*.zst | sed -E 's#.*/\w{2,3}_([0-9]+)\.jsonl\.zst#\1#' | sort -n | tail -1)
-elif [ $# -gt 2 ] && [ "$INDEX" == "failed" ]; then
+elif [ $# -gt 2 ] && [ "$2" == "failed" ]; then
 # Select only failed jobs (timeout, oom and failed status)
 # Create a list of batch id's separated by comma
     JOB=$3
-    INDEX=$(
+    INDEX=$(\
         sacct -j $JOB --parsable -s oom,f,to -n \
-        | grep -v '.clean' \
+        | grep -v '.batch' \
         | sed -E 's/[0-9]+_([0-9]+)\|.*/\1/g' \
         | paste -sd','
     )
@@ -28,7 +28,7 @@ else
     INDEX=$2
 fi
 
-echo "Job array of index $INDEX"
+echo "Job array of index $INDEX for $L"
 read -p "Confirm? [y/n] " -n 1 -r
 if [[ ! $REPLY =~ [Yy] ]]; then echo; exit 1; fi
 echo
