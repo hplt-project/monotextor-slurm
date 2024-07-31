fd4e7d923e7ce35aec8417703558c22f7a11cba1
ZJaume <jzaragoza@prompsit.com>
2024-07-31 11:56:32 +0300

    Update status script to the new pipeline structure

--- status.sh
+++ status.sh
@@ -10,51 +10,29 @@ colls=$(echo $colls | tr ' ' '\n' | sort)
 for c in $colls; do
     if [[ $c =~ sample ]]; then continue; fi
 
-    dir=$WORKSPACE/$c/$L
-
-    running=$(ls -1 $dir/batch.* | grep tmp | wc -l)
-    nbatches=$(ls -1 $dir/batch.* | wc -l)
-    scored=$(ls -1 $dir/scored.*.jsonl.* | grep -c -v tmp_)
-    scored_tmp=$(ls -1 $dir/scored.*.jsonl.* | grep -c tmp_)
-    total=$(ls -1 $dir/scored.*.jsonl.* | wc -l)
-    if [[ $nbatches -eq 0 ]] && [[ $scored -eq 0 ]]; then
+    dir=$WORKSPACE/batches/$c/$L
+    nbatches=$(ls -1 $dir/batch_*.jsonl.zst | wc -l)
+    if [[ $nbatches -eq 0 ]]; then
         printf "none\t"
         continue
-    elif [[ $running -ne 0 ]]; then
-        printf "batching\t"
-        continue
     fi
 
-    if [[ $total -eq 0 ]]; then
+    dir=$WORKSPACE/dedup/$c/$L
+    dedup=$(ls  -1 $dir/batch_*.jsonl.zst | wc -l)
+    dedup_tmp=$(ls  -1 $dir/batch_*.jsonl.zst.tmp | wc -l)
+    if [[ $dedup -eq 0 ]] || [[ $dedup_tmp -ne 0 ]]; then
         printf "batched\t"
         continue
-    elif [[ $nbatches -ne 0 ]] && [[ $scored -ne $nbatches ]]; then
-        printf "scoring $scored/$nbatches\t"
-        continue
     fi
 
-    dir=$WORKSPACE/dedup/$L
-
-    dedup_tmp=$(ls -1 $dir/*.jsonl.zst.tmp | wc -l)
-    if ! [ -f $dir/${L}_1.jsonl.zst ] || ! [ $dedup_tmp -eq 0 ]
-    then
-        printf "scored\t"
+    dir=$WORKSPACE/annotated/$c/$L
+    scored=$(ls  -1 $dir/batch_*.jsonl.zst | wc -l)
+    scored_tmp=$(ls  -1 $dir/batch_*.jsonl.zst.tmp | wc -l)
+    if [[ $scored -eq 0 ]] || [[ $scored_tmp -ne 0 ]]; then
+        printf "deduped\t"
         continue
-    fi
-
-    dir=$WORKSPACE/clean/$L
-
-    clean_tmp=$(ls -1 $dir/*.jsonl.zst.tmp | wc -l)
-    stats=$(ls -1 $dir/${L}_stats | wc -l)
-    if [ -f $dir/${L}_1.jsonl.zst ] && [ $clean_tmp -eq 0 ] && [ $stats -eq 1 ]
-    then
-        printf "cleaned\tcleaned\tcleaned\tcleaned\n"
-        exit 0
     else
-        printf "deduped\tdeduped\tdeduped\tdeduped\n"
-        exit 0
+        printf "annotated\t"
     fi
-
-    printf "\t"
-done
+done 2>/dev/null
 echo
