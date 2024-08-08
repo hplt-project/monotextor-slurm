cf68697ecd93e0fbcbdff82bf744cdbc4209c859
ZJaume <jzaragoza@prompsit.com>
2024-08-08 09:57:50 +0000

    robots: multi-threaded processing and fixed query escaping

--- utils/src/bin/robots-filter.rs
+++ utils/src/bin/robots-filter.rs
@@ -9,16 +9,20 @@
  * user-agents, for example) to the input file of this program.
  */
 use std::io::{self,BufRead};
+use std::sync::mpsc::{Sender, channel};
+use std::sync::{Arc, RwLock};
+use std::collections::HashSet;
 use std::fs;
 
+use indicatif::{ParallelProgressIterator, ProgressBar, ProgressStyle, ProgressFinish};
 use fst::{Set, IntoStreamer, Streamer};
 use patricia_tree::StringPatriciaSet;
 use regex_automata::dense;
+use rayon::prelude::*;
 use env_logger::Env;
-use memmap2::Mmap;
 use regex::Regex;
 use clap::Parser;
-use log::debug;
+use log::{debug, warn};
 
 
 #[derive(Parser)]
@@ -27,31 +31,59 @@ struct Args {
     #[clap(help="FST indexed URLs to search in")]
     indexpath: String,
     #[clap(help="List of allowance URL patterns from robots.txt")]
-    allowancepath: String,
+    allowancefiles: Vec<String>,
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
-    let args = Args::parse();
+struct SharedState {
+    index: Arc<Set<Vec<u8>>>,
+    sender: Sender<String>,
+    banned: Arc<RwLock<HashSet<String>>>,
+}
 
-    // Regex for escaping sequences in the allowance patterns
-    // robots.txt only supports * and $
-    // so the rest of the symbols have to be interpreted as literals
-    let escaper = Regex::new(r"[\-\.\+\?\(\)\[\]\{\}\\]")?;
-    // converts '*' to '.*'
-    let wildcard_convert = Regex::new(r"\*")?;
-    // removes unsupported anchors by FST
-    let remove_anchors = Regex::new(r"[\^\$]")?;
+impl Clone for SharedState {
+    fn clone(&self) -> Self {
+        SharedState {
+            index: self.index.clone(),
+            sender: self.sender.clone(), // Clone the sender for thread safety
+            banned: self.banned.clone(),
+        }
+    }
+}
+
+// escape metasequences characters like regex_syntax::escape
+// except *, which is converted to .*
+// and anchors are removed
+pub fn is_meta_character(c: char) -> bool {
+    match c {
+        '\\' | '.' | '+' | '?' | '(' | ')' | '|' | '[' | ']' | '{'
+        | '}' | '#' | '&' | '-' | '~' => true,
+        _ => false,
+    }
+}
+pub fn escape(text: &str) -> String {
+    let mut buf = String::with_capacity(text.len());
+    for c in text.chars() {
+        if c == '^' || c == '$' {
+            continue
+        } else if c == '*' {
+            buf.push('.');
+        } else if is_meta_character(c) {
+            buf.push('\\');
+        }
+        buf.push(c);
+    }
+    buf
+}
+
+
+fn process_file(state: &mut SharedState, filepath: &str) -> Result<(), Box<dyn std::error::Error + 'static>> {
     // check if $ anchor at the end
-    let anchor_end = Regex::new(r"\$$")?;
+    let anchor_end = Regex::new(r"[\$\*]$")?;
     // extract domain from url
     let domain_re = Regex::new(r"^(https?://)?(www\.)?([^/]+)?.*$")?;
 
-    let mmap = unsafe { Mmap::map(&fs::File::open(args.indexpath)?)? };
-    let index = Set::new(mmap)?;
-
     // Open iterator over the lines of the dissallowed list
-    let file = fs::File::open(args.allowancepath)?;
+    let file = fs::File::open(filepath)?;
     let lines = io::BufReader::new(file).lines();
 
     // asumes URLs coming grouped by each robots file
@@ -59,6 +91,7 @@ fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
     // wee keep the domain name and the disallowed urls for that domain
     let mut cur_domain = String::with_capacity(500);
     let mut cur_allowance: StringPatriciaSet = StringPatriciaSet::new();
+    let mut warned_size = false;
 
     for (i, line_result) in lines.enumerate() {
         // parse tab serparted lines
@@ -83,22 +116,24 @@ fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
             cur_domain.push_str(domain);
             debug!("New domain! {cur_domain}");
             for u in cur_allowance.iter() {
-                println!("{}", u);
+                state.sender.send(format!("{}", u))?;
             }
-            cur_allowance.clear();
+            cur_allowance = StringPatriciaSet::new();
+            warned_size = false;
+        }
+        if state.banned.read().unwrap().contains(&cur_domain) {
+            continue;
         }
 
         // Escape regex characters that are not supported
-        let escaped = escaper.replace_all(&url, r"\$0");
-        let escaped_wildcard = wildcard_convert.replace_all(&escaped, r".*");
-        let removed_anchors = remove_anchors.replace_all(&escaped_wildcard, "");
+        let escaped = escape(&url);
 
         // If the url had $ at the end, do not add the .* suffix
         let query;
         if !anchor_end.is_match(&url) {
-            query = format!("{removed_anchors}.*");
+            query = format!("{escaped}.*");
         } else {
-            query = removed_anchors.to_string();
+            query = escaped.to_string();
         }
         debug!("Escaped url: {query}");
 
@@ -109,23 +144,80 @@ fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
             .byte_classes(true)
             .premultiply(true)
             .reverse(false)
-            .build(&query)?;
+            .build(&query).expect(format!("Escaped '{}' url '{}'", query, url).as_str());
         let dfa = match dense_dfa {
             dense::DenseDFA::PremultipliedByteClass(dfa) => dfa,
             _ => unreachable!(),
         };
         // search over the FST
-        let mut results = index.search(dfa).into_stream();
+        let mut results = state.index.search(dfa).into_stream();
+        let mut num_results = 0;
         // iterate over the results
         // for those allowed, remove the url from the set in case was added by a previous query
         // for those disallowed, add them to the set for the current domain
         while let Some(key) = results.next() {
+            num_results += 1;
             if allowed {
                 cur_allowance.remove(&String::from_utf8(key.to_vec())?);
             } else {
-                cur_allowance.insert(String::from_utf8(key.to_vec())?);
+                // if the allowance list becomes too large, stop adding urls to it
+                if cur_allowance.len() > 500_000 {
+                    state.sender.send(format!("{}", String::from_utf8(key.to_vec())?))?;
+                    if !warned_size {
+                        warn!("The in-memory list reached size limit for query '{}' in file '{}'", escaped, filepath);
+                        warned_size = true;
+                    }
+                } else {
+                    cur_allowance.insert(String::from_utf8(key.to_vec())?);
+                }
             }
         }
+        if num_results > 5_000_000 {
+            warn!("Very big query results '{}' with '{}'", num_results, url);
+            state.banned.write().unwrap().insert(cur_domain.clone());
+        }
     }
+
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    let args = Args::parse();
+
+    let progstyle = ProgressStyle::default_bar();
+    let progbar = ProgressBar::new(args.allowancefiles.len() as u64)
+        .with_style(progstyle)
+        .with_finish(ProgressFinish::AndLeave);
+
+    // let mmap = unsafe { Mmap::map(&fs::File::open(args.indexpath)?)? };
+    let index = Arc::new(Set::new(fs::read(args.indexpath)?)?);
+
+    let (sender, receiver) = channel();
+    let shared_state = SharedState {
+        index: index.clone(),
+        sender: sender,
+        banned: Arc::new(RwLock::new(HashSet::new())),
+    };
+
+    // process each file in parallel, sharing the same index
+    let handler = std::thread::spawn(move || {
+        args.allowancefiles
+            .par_iter()
+            .progress_with(progbar)
+            .for_each_with(shared_state.clone(), |state, filename| {
+                process_file(state, &filename).unwrap();
+            });
+
+        // explicitly drop the shared state, so we make sure there are no remaining senders
+        // after the par_iter finishes and therefore the receiver does not deadblock
+        drop(shared_state);
+    });
+
+    while let Ok(line) = receiver.recv() {
+        println!("{}", line);
+    }
+    handler.join().unwrap();
+
     Ok(())
 }
