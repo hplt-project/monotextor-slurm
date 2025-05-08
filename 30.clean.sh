11cddefccb429a0017d8da0632021447f2e388c5
ZJaume <jzaragoza@prompsit.com>
2025-05-08 13:34:55 +0300

    Use a single log file for HQ workers

--- 30.clean.sh
+++ 30.clean.sh
@@ -20,7 +20,7 @@ hq alloc add slurm --name clean \
     --idle-timeout $idle_timeout --time-limit 72h \
     -- -p small -A $SBATCH_ACCOUNT \
     --cpus-per-task 128 --ntasks 1 --mem-per-cpu 1750 \
-    -o "$SLURM_LOGS_DIR/hq-worker-%x.out" -e "$SLURM_LOGS_DIR/hq-worker-%x.err"
+    -o "$SLURM_LOGS_DIR/hq-worker-%x.log"
 # obtain the allocation queue id
 qid=$(hq alloc list --output-mode json | jq -cr ".[] | select(.name == \"clean\") | .id" | head -1)
 
