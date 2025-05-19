6d04b52d28caffa20a1d92014e4147f004540fb9
ZJaume <jzaragoza@prompsit.com>
2025-05-19 16:54:14 +0300

    Add html_lang and xml

--- scripts/merge-meta-text.py
+++ scripts/merge-meta-text.py
@@ -6,6 +6,7 @@ import io
 collection = sys.argv[1]
 input_dir = sys.argv[2]
 output_file = sys.argv[3]
+
 level = 10
 # doing compression in separated threads is much faster
 compressor = zstandard.ZstdCompressor(level=level, threads=4)
@@ -33,6 +34,10 @@ with zstandard.open(f'{input_dir}/text.zst', 'rt', errors='strict') as text_file
         doc["collection"] = collection
         doc.update(lang)
         doc["text"] = text["t"] #insert the text at the end of the json
+        if "x" in text:
+            doc["xml"] = text["x"]
+        if "htmllang" in text:
+            doc["html_lang"] = text["htmllang"]
 
         # write bytes directly
         out_file.write(orjson.dumps(doc))
